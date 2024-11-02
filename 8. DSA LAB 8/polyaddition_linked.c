724b4e2b56f91f14ad09058aded40150c7c9d721
Karthik Krishnan <141357296+iamkarthik2004@users.noreply.github.com>
2024-11-02 14:32:28 +0530

    Update polyaddition_linked.c

--- 8. DSA LAB 8/polyaddition_linked.c
+++ 8. DSA LAB 8/polyaddition_linked.c
@@ -1,172 +1,114 @@
-#include<stdio.h>
-#include<stdlib.h>
-
-struct node
+#include <stdio.h>
+#include <stdlib.h>
+struct Node
 {
-  int data;
-  struct node* link;
+ int coeff;
+ int exp;
+ struct Node* next;
+};
+struct Node* createNode(int coeff, int exp)
+ {
+  struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+  newNode->coeff = coeff;
+  newNode->exp = exp;
+  newNode->next = NULL;
+  return newNode;
+ }
+void insertNode(struct Node** poly, int coeff, int exp)
+{
+  struct Node* newNode = createNode(coeff, exp);
+  if (*poly == NULL)
+ {
+  *poly = newNode;
+ }
+else
+ {
+  struct Node* temp = *poly;
+  while (temp->next != NULL)
+ {
+   temp = temp->next;
+ }
+  temp->next = newNode;
+ }
 }
-*temp,*head,*ptr,*ptr1;
-
-void main(){
-int ch,item,k;
-do{
-  printf("\n1.Insertion at begining\n2.Insertion at end\n3.Insertion at specific postion\n4.deletion at begining\n");
-  printf("5.Deletion at end\n6.Deletion at specific position\n7.Display\n8.Exit\n");
-  printf("Enter the choice: ");
-  scanf(" %d",&ch);
-  switch(ch){
-  case 1:
-  printf("Enter the item: ");
-  scanf(" %d",&item);
-  temp=(struct node*)malloc(sizeof(struct node));
-  if(temp==NULL){
-  printf("Underflow");
+ void displayPolynomial(struct Node* poly)
+{
+  while (poly != NULL)
+  {
+   printf("%dx^%d", poly->coeff, poly->exp);
+   if (poly->next != NULL && poly->next->coeff >= 0) {
+   printf(" + ");
   }
-  else if(head==NULL){
-  temp->data=item;
-  temp->link=NULL;
-  head=temp;
+ poly = poly->next;
+ }
+ printf("\n");
+}
+struct Node* addPolynomials(struct Node* poly1, struct Node* poly2)
+ {
+  struct Node* result = NULL;
+  while (poly1 != NULL && poly2 != NULL)
+ {
+  if (poly1->exp > poly2->exp)
+  {
+   insertNode(&result, poly1->coeff, poly1->exp);
+   poly1 = poly1->next;
   }
-  else{
-  temp->data=item;
-  temp->link=head;
-  head=temp;
+else if (poly1->exp < poly2->exp)
+  {
+   insertNode(&result, poly2->coeff, poly2->exp);
+   poly2 = poly2->next;
   }
-  break;
-  case 2:
-  printf("Enter the item: ");
-  scanf(" %d",&item);
-  temp=(struct node*)malloc(sizeof(struct node));
-  if(temp==NULL){
-  printf("Underflow");
+else
+ {
+   int sumCoeff = poly1->coeff + poly2->coeff;
+   if (sumCoeff != 0)
+  {
+   insertNode(&result, sumCoeff, poly1->exp);
   }
-  else if(head==NULL){
-  temp->data=item;
-  temp->link=NULL;
-  head=temp;
-  }
-  else{
-  ptr=head;
-  while((ptr->link)!=NULL){
-  ptr=ptr->link;
-  }
-  ptr->link=temp;
-  temp->link=NULL;
-  temp->data=item;
-  }
-  break;
-case 3:
-printf("Enter the item: ");
-scanf(" %d",&item);
-printf("Enter the item after which value to be placed: ");
-scanf("%d",&k);
-temp=(struct node*)malloc(sizeof(struct node));
-if(temp==NULL){
-printf("Underflow");
+  poly1 = poly1->next;
+  poly2 = poly2->next;
+ }
 }
-else if(head==NULL){
-printf("Insertion not possible");
+while (poly1 != NULL)
+ {
+   insertNode(&result, poly1->coeff, poly1->exp);
+   poly1 = poly1->next;
+ }
+while (poly2 != NULL)
+ {
+  insertNode(&result, poly2->coeff, poly2->exp);
+  poly2 = poly2->next;
+ }
+ return result;
 }
-else{
-ptr=head;
-while((ptr->data)!=k && (ptr->link)!=NULL){
-ptr=ptr->link;
-}
-if(ptr->link==NULL){
-printf("\nSearch fails");
-}
-else{
-temp->link=ptr->link;
-ptr->link=temp;
-temp->data=item;
-}
-}
-break;
-case 4:
-if(head==NULL){
-printf("Underflow");\
-}
-else{
-ptr=head;
-if(ptr->link==NULL){
-head=NULL;
-free(ptr);
-}
-else{
-head=ptr->link;
-free(ptr);
-}
-printf("\nNode deleted");
-}
-break;
-case 5:
-if(head==NULL){
-printf("\nUnderflow");
-}
-else{
-ptr=head;
-ptr1=ptr->link;
-while((ptr1->link)!=NULL){
-ptr=ptr->link;
-ptr1=ptr1->link;
-}
-ptr->link=NULL;
-free(ptr1);
-printf("Node deleted from end");
-}
-break;
-case 6:
-if(head==NULL){
-printf("\nUnderflow");
-}
-else{
-ptr=head;
-printf("Enter the item after which have to be deleted: ");
-scanf("%d",&k);
-if(ptr->link==NULL && ptr->data==k){
-printf("There is no node to be deleted");
-}
-else{
-ptr1=ptr->link;
-while((ptr->link)!=NULL && (ptr->data)!=k){
-ptr=ptr->link;
-ptr1=ptr1->link;
-
-}
-if(ptr->link==NULL){
-printf("\nElement not found");
-}
-else{
-ptr->link=ptr1->link;
-free(ptr1);
-printf("Node is deleted");
-}
-}
-
-}
-break;
-case 7:
-ptr=head;
-if(ptr==NULL){
-printf("List is empty");
-}
-else{
-while((ptr->link)!=NULL){
-printf("%d ",ptr->data);
-ptr=ptr->link;
-}printf("%d ",ptr->data);
-}
-
-break;
-case 8:
-printf("Exiting....\n");
-break;
-default:
-printf("\nInvalid option");
-break;
-}
-
-}while(ch!=8);
-
-}
+int main()
+ {
+  struct Node* poly1 = NULL;
+  struct Node* poly2 = NULL;
+  struct Node* sum = NULL;
+  int n, coeff, exp;
+  printf("Enter the number of terms in the first polynomial: ");
+  scanf("%d", &n);
+  printf("Enter the terms in the format (coefficient exponent):\n");
+ for (int i = 0; i < n; i++)
+ {
+  scanf("%d %d", &coeff, &exp);
+  insertNode(&poly1, coeff, exp);
+ }
+ printf("Enter the number of terms in the second polynomial:");
+ scanf("%d", &n);
+ printf("Enter the terms in the format (coefficient exponent):\n");
+for (int i = 0; i < n; i++)
+ {
+  scanf("%d %d", &coeff, &exp);
+  insertNode(&poly2, coeff, exp);
+ }
+  printf("\nFirst Polynomial: ");
+  displayPolynomial(poly1);
+  printf("Second Polynomial: ");
+  displayPolynomial(poly2);
+  sum = addPolynomials(poly1, poly2);
+  printf("\nSum of the two polynomials: ");
+  displayPolynomial(sum);
+  return 0;
+ }
